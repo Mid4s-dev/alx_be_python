67116c5d7eb8a54b582953ed2d22d2d23f8b1109
mid4s-dev <lugayajoshua@gmail.com>
2025-10-06 00:37:18 +0300

    fix here and there

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -3,66 +3,31 @@ from simple_calculator import SimpleCalculator
 
 
 class TestSimpleCalculator(unittest.TestCase):
-    """Unit tests for the SimpleCalculator class."""
 
     def setUp(self):
-        """Create a calculator instance before each test."""
         self.calc = SimpleCalculator()
 
-    # --- Addition Tests ---
-    def test_addition_with_positive_numbers(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(2, 3), 5)
-        self.assertEqual(self.calc.add(10, 15), 25)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(0, 0), 0)
 
-    def test_addition_with_negative_numbers(self):
-        self.assertEqual(self.calc.add(-2, -3), -5)
-        self.assertEqual(self.calc.add(-5, 5), 0)
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(3, 5), -2)
+        self.assertEqual(self.calc.subtract(0, 0), 0)
 
-    def test_addition_with_floats(self):
-        self.assertAlmostEqual(self.calc.add(2.5, 3.1), 5.6)
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(-1, 3), -3)
+        self.assertEqual(self.calc.multiply(0, 5), 0)
 
-    # --- Subtraction Tests ---
-    def test_subtraction_with_positive_numbers(self):
-        self.assertEqual(self.calc.subtract(10, 5), 5)
-
-    def test_subtraction_with_negative_numbers(self):
-        self.assertEqual(self.calc.subtract(-5, -5), 0)
-        self.assertEqual(self.calc.subtract(-5, 5), -10)
-
-    def test_subtraction_with_floats(self):
-        self.assertAlmostEqual(self.calc.subtract(5.5, 2.2), 3.3)
-
-    # --- Multiplication Tests ---
-    def test_multiplication_with_positive_numbers(self):
-        self.assertEqual(self.calc.multiply(4, 5), 20)
-
-    def test_multiplication_with_negative_numbers(self):
-        self.assertEqual(self.calc.multiply(-4, 5), -20)
-        self.assertEqual(self.calc.multiply(-4, -5), 20)
-
-    def test_multiplication_with_zero(self):
-        self.assertEqual(self.calc.multiply(0, 10), 0)
-
-    def test_multiplication_with_floats(self):
-        self.assertAlmostEqual(self.calc.multiply(2.5, 4.0), 10.0)
-
-    # --- Division Tests ---
-    def test_division_with_positive_numbers(self):
-        self.assertEqual(self.calc.divide(10, 2), 5)
-
-    def test_division_with_negative_numbers(self):
-        self.assertEqual(self.calc.divide(-10, 2), -5)
-        self.assertEqual(self.calc.divide(-10, -2), 5)
-
-    def test_division_with_floats(self):
-        self.assertAlmostEqual(self.calc.divide(7.5, 2.5), 3.0)
-
-    def test_division_by_zero(self):
-        self.assertIsNone(self.calc.divide(10, 0))
-
-    def test_division_zero_by_number(self):
-        self.assertEqual(self.calc.divide(0, 5), 0)
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(6, 3), 2)
+        self.assertEqual(self.calc.divide(-6, 3), -2)
+        with self.assertRaises(ZeroDivisionError):
+            self.calc.divide(5, 0)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
